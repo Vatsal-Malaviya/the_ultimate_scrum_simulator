43917a9da518361bb6bac913072921263c80e5b2
Vatsal Malaviya <77090011+Vatsal-Malaviya@users.noreply.github.com>
2023-10-25 00:23:09 -0700

    Adding Starter Code with Maven and SpringBoot configuration
    * feat: added starter code with maven and spring-boot

---
+++ src/main/java/in/ser/the_ultimate_scrum_simulator/TheUltimateScrumSimulatorApplication.java
@@ -0,0 +1,13 @@
+package in.ser.the_ultimate_scrum_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheUltimateScrumSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheUltimateScrumSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/ser/the_ultimate_scrum_simulator/TheUltimateScrumSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package in.ser.the_ultimate_scrum_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheUltimateScrumSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
