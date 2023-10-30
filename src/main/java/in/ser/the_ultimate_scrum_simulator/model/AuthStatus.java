7230223eddeafb0c24f2ef06298be9e97219863b
ajain342 <ajain342@asu.edu>
2023-10-29 23:58:56 -0700

    Taiga task #50 US #49

---
+++ src/main/java/in/ser/the_ultimate_scrum_simulator/model/AuthStatus.java
@@ -0,0 +1,9 @@
+package in.ser.the_ultimate_scrum_simulator.model;
+
+public enum AuthStatus {
+    SUCCESS,
+    UNKNOWN_ERROR,
+    USER_NOT_FOUND,
+    INCORRECT_PASSWORD,
+    TOO_MANY_FAILED_LOGINS
+}
