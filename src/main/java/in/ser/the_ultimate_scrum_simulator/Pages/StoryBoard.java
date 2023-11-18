5184e0d5e7b1bf269a58d8cd5ca6a9182ed35069
sramara6 <144416789+sramara6@users.noreply.github.com>
2023-11-17 19:44:46 -0700

    Implemented story board UI us18 t82 (#37)
    * feat: Story Board UI TG-82
    
    * feat:Additional UI components- Story Board UI TG-82

---
+++ src/main/java/in/ser/the_ultimate_scrum_simulator/Pages/StoryBoard.java
@@ -0,0 +1,71 @@
+package in.ser.the_ultimate_scrum_simulator.Pages;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class StoryBoard extends JPanel {
+    private JFrame parentFrame;
+    private JPanel backlogPanel, todoPanel, inProgressPanel, completedPanel;
+
+    public StoryBoard(JFrame frame, String role) {
+        this.parentFrame = frame;
+        this.setLayout(new BorderLayout());
+
+        backlogPanel = createSwimlane("BACKLOG");
+        todoPanel = createSwimlane("TO DO");
+        inProgressPanel = createSwimlane("IN PROGRESS");
+        completedPanel = createSwimlane("COMPLETED");
+
+
+        JPanel swimlanesPanel = new JPanel(new GridLayout(1, 4, 10, 0));
+        swimlanesPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        swimlanesPanel.add(backlogPanel);
+        swimlanesPanel.add(todoPanel);
+        swimlanesPanel.add(inProgressPanel);
+        swimlanesPanel.add(completedPanel);
+        JLabel titleLabel = new JLabel("SCRUM BOARD", SwingConstants.CENTER);
+        titleLabel.setFont(new Font("Arial", Font.BOLD, 24));
+        this.add(titleLabel, BorderLayout.NORTH);
+        this.add(swimlanesPanel, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        buttonPanel.add(createButton("Go Back", e -> openPlayGameViewInstructions(role)));
+        buttonPanel.add(createButton("Create Burndown Chart", null));
+        buttonPanel.add(createButton("Create Velocity Chart", null));
+        this.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    private JButton createButton(String text, ActionListener actionListener) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, 14));
+        if (actionListener != null) {
+            button.addActionListener(actionListener);
+        }
+        return button;
+    }
+
+    public void openPlayGameViewInstructions(String role) {
+        parentFrame.getContentPane().removeAll();
+        parentFrame.add(new PlayGameViewInstructions(parentFrame, role));
+        parentFrame.revalidate();
+        parentFrame.repaint();
+    }
+    private JPanel createSwimlane(String title) {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBorder(BorderFactory.createTitledBorder(title));
+        return panel;
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Kanban Board");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(800, 600));
+        //frame.add(new StoryBoard(frame, role));
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
