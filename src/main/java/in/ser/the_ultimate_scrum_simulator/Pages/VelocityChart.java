f2432c0b26720ebf907a68132dfe78258f196cbe
ajain342 <ajain342@asu.edu>
2023-11-24 13:44:39 -0700

    Taiga task #86 - complete velocityChart

--- src/main/java/in/ser/the_ultimate_scrum_simulator/Pages/VelocityChart.java
+++ src/main/java/in/ser/the_ultimate_scrum_simulator/Pages/VelocityChart.java
@@ -51,7 +51,46 @@ public class VelocityChart extends JFrame {
         repaint(); // Redraw the chart with updated data
     }
 
+    private class ChartPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
 
+            // Calculate bar width and spacing
+            int barWidth = getWidth() / velocities.length;
+            int spacing = 10;
+
+            // Draw x-axis
+            g.drawLine(50, getHeight() - 50, getWidth() - 50, getHeight() - 50);
+
+            // Draw y-axis
+            g.drawLine(50, 50, 50, getHeight() - 50);
+
+            // Draw y-axis title
+            g.drawString("Velocity", 20, getHeight() / 2);
+
+            // Draw x-axis title
+            g.drawString("Sprints", getWidth() / 2, getHeight() - 20);
+
+            // Draw bars for each sprint
+            for (int i = 0; i < velocities.length; i++) {
+                int barHeight = velocities[i] * 5; // Scaling factor for better visualization
+                int x = 50 + i * (barWidth + spacing);
+                int y = getHeight() - 50 - barHeight;
+
+                // Draw bars
+                g.setColor(Color.BLUE);
+                g.fillRect(x, y, barWidth, barHeight);
+
+                // Draw x-axis values
+                g.setColor(Color.BLACK);
+                g.drawString(sprintNames[i], x + barWidth / 2 - 20, getHeight() - 30);
+
+                // Draw y-axis values
+                g.drawString(Integer.toString(velocities[i]), x + barWidth / 2 - 5, y - 5);
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
