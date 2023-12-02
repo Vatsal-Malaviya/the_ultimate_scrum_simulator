a80d7e450e6fe20cad75066385a294f4eba13ac7
Ansh <aagra109@asu.edu>
2023-12-01 20:39:32 -0700

    feat: api for delete scenario TG-130

--- src/main/java/in/ser/the_ultimate_scrum_simulator/DbWrapper.java
+++ src/main/java/in/ser/the_ultimate_scrum_simulator/DbWrapper.java
@@ -274,5 +274,29 @@ public class DbWrapper {
         return scenarioIds;
     }
 
+    public ScenarioDeleteStatus deleteScenarioById(int scenarioId) {
+        try {
+            if (scenarioId <= 0) {
+                return ScenarioDeleteStatus.INVALID_SCENARIO_ID;
+            }
+
+            String sql = "DELETE FROM scenario WHERE id = ?";
+            try (PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setInt(1, scenarioId);
+
+                int rowsAffected = ps.executeUpdate();
+
+                if (rowsAffected > 0) {
+                    return ScenarioDeleteStatus.SUCCESS;
+                } else {
+                    return ScenarioDeleteStatus.SCENARIO_NOT_FOUND;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return ScenarioDeleteStatus.UNKNOWN_ERROR;
+        }
+    }
+
 
 }
